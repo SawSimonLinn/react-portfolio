ed7b305ad8130e14e7b523f29a0bbb3ae109860b
Simon <sawsimonelinn@gmail.com>
2025-02-16 13:07:40 -0800

    new update

--- src/Components/MyWork/MyWork.jsx
+++ src/Components/MyWork/MyWork.jsx
@@ -11,31 +11,24 @@ const MyWork = () => {
       <div className='mywork-container'>
         {mywork_data.map((work, index) => {
           return (
-            <a
-              key={index}
-              href={work.w_url}
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <div className='work-card'>
-                <img src={work.w_img} alt={work.w_name} />
-                <div className='work-title'>
-                  <h2>{work.w_name}</h2>
-                </div>
-                <div className='work-info'>
-                  <h2>{work.w_name}</h2>
-                  <p>{work.w_description}</p>
-                  <div className='work-btn'>
-                    <a href={work.w_url} target='_blank'>
-                      <button>Demo</button>
-                    </a>
-                    <a href={work.w_github} target='_blank'>
-                      <button>GitHub</button>
-                    </a>
-                  </div>
+            <div className='work-card'>
+              <img src={work.w_img} alt={work.w_name} />
+              <div className='work-title'>
+                <h2>{work.w_name}</h2>
+              </div>
+              <div className='work-info'>
+                <h2>{work.w_name}</h2>
+                <p>{work.w_description}</p>
+                <div className='work-btn'>
+                  <a href={work.w_url} target='_blank'>
+                    <button>Demo</button>
+                  </a>
+                  <a href={work.w_github} target='_blank'>
+                    <button>GitHub</button>
+                  </a>
                 </div>
               </div>
-            </a>
+            </div>
           );
         })}
       </div>
